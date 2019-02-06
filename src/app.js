4efdd1b3251884d52bef7e83d5c8acf9af23759c
volosgoto <volosgoto@yahoo.com>
2019-02-06 11:48:24 +0200

    add routs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<p>This is boilerpalte</p>, document.getElementById('app'))
+
+{/* <Route path component/> */}
+
+const ExpendsDashboardPage = ()=> (<div>!!!!This is from ExpendsDashboardPage!!!</div>);  
+const AddExpendsPage = ()=> (<div>AddExpendsPage</div>);  
+const EditExpendsPage = ()=> (<div>EditExpendsPage</div>);  
+const HelpExpendsPage = ()=> (<div>HelpExpendsPage</div>);  
+
+/**exact={true} only matches*/
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={ExpendsDashboardPage} exact={true}/> 
+      <Route path='/create' component={AddExpendsPage} />
+      <Route path='/edit' component={EditExpendsPage} />
+      <Route path='/help' component={HelpExpendsPage} />
+    </div>
+  </BrowserRouter>
+);
+      
+ReactDOM.render(routes, document.getElementById('app'));
