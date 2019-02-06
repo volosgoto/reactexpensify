f1603af6e37e6261e2b264098ce298ea10039b20
volosgoto <volosgoto@yahoo.com>
2019-02-06 12:22:23 +0200

    add 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -11,17 +11,20 @@ const ExpendsDashboardPage = ()=> (<div>!!!!This is from ExpendsDashboardPage!!!
 const AddExpendsPage = ()=> (<div>AddExpendsPage</div>);  
 const EditExpendsPage = ()=> (<div>EditExpendsPage</div>);  
 const HelpExpendsPage = ()=> (<div>HelpExpendsPage</div>);  
+const NotFoundPage = ()=> (<div>404. NotFoundPage</div>);  
 
 /**exact={true} only matches*/
+// Switch for routs thet aren't defined
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path='/' component={ExpendsDashboardPage} exact={true}/> 
       <Route path='/create' component={AddExpendsPage} />
       <Route path='/edit' component={EditExpendsPage} />
       <Route path='/help' component={HelpExpendsPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
       
