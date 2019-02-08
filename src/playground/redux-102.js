d90a56c9772fb5dca920cfa581dfef00e2536d5f
volosgoto <volosgoto@yahoo.com>
2019-02-08 10:20:10 +0200

    redux dymamoc actions

---
+++ src/playground/redux-102.js
@@ -0,0 +1,79 @@
+import { createStore } from "redux";
+
+// Set first time redux state by default value
+const store = createStore((state = { count: 0 }, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      let increment = (typeof action.incrementBy === 'number') ? action.incrementBy : 1;
+      return { count: state.count + increment };
+    case 'DECREMENT':
+      let decrement = (typeof action.decrementBy === 'number') ? action.decrementBy : 1;
+      return { count: state.count - decrement };  
+    case 'RESET':
+      return {count: state.count = 0}; 
+    case 'SET':
+      return { 
+        count: action.count // 101
+      }  
+    default: 
+      return state;  
+  }
+  
+  // if (action.type == 'INCREMENT'){
+  //   return {
+  //     count: state.count + 1
+  //   };
+  // }
+  // return state; 
+});
+
+// Fetch current state
+// console.log(store.getState());
+
+
+// Watch for changes of redux store state
+let unsubscribe = store.subscribe(()=>{
+  console.log(store.getState());
+});
+
+// ACTIONS - object for change store
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101 // action.count
+});
+
+
+unsubscribe(); // stops the subscription
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 322 // action.count
+});
+
