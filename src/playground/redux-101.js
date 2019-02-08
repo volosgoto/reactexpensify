b9b2988dc04dca9cd51b0fcd1e1c7a1251796974
volosgoto <volosgoto@yahoo.com>
2019-02-08 09:25:47 +0200

    redux actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,46 @@
 import { createStore } from "redux";
 
 // Set first time redux state by default value
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };  
+    case 'RESET':
+      return {count: state.count = 0}; 
+    default: 
+      return state;  
+  }
+  
+  // if (action.type == 'INCREMENT'){
+  //   return {
+  //     count: state.count + 1
+  //   };
+  // }
+  // return state; 
 });
 
 // Fetch current state
 console.log(store.getState());
 
-// ACTIONS
+
+// ACTIONS - object for change store
+// increment, decrement, reset
+store.dispatch({
+    type: 'INCREMENT'
+});
+store.dispatch({
+  type: 'DECREMENT'
+});
+store.dispatch({
+  type: 'DECREMENT'
+});
+store.dispatch({
+  type: 'DECREMENT'
+});
+store.dispatch({
+  type: 'RESET'
+});
+
+console.log(store.getState());
