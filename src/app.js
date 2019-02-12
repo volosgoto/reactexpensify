fab85ea78dbe4f36f89272f3ba4f47eeb31b300f
volosgoto <volosgoto@yahoo.com>
2019-02-12 14:34:34 +0200

    ExpenseList+

--- src/app.js
+++ src/app.js
@@ -11,18 +11,17 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
-
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'));
-}, 3000);
-
+  store.dispatch(setTextFilter('bill'));
+}, 2000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
 console.log(visibleExpenses);
 
 const jsx = (
